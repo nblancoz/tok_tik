ca77b13f71c5d7ab655dfd8b50736b45e6702c07
nblancoz <nblancozappala@gmail.com>
2024-08-02 21:00:55 +0200

    Wip: added like & views buttons

--- lib/presentation/providers/discover_provider.dart
+++ lib/presentation/providers/discover_provider.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:tok_tik/domain/entities/video_post.dart';
+import 'package:tok_tik/infrastructure/models/local_video_model.dart';
+import 'package:tok_tik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
   bool initialLoading = true;
   List<VideoPost> videos = [];
 
   Future<void> loadNextPage() async {
-    // todo: cargar videos
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<VideoPost> newVideos = videoPosts
+        .map((video) => LocalVideoModel.fromJson(video).toVideoPostEntity())
+        .toList();
+
+    videos.addAll(newVideos);
+    initialLoading = false;
     notifyListeners();
   }
 }
---
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:tok_tik/domain/entities/video_post.dart';
+import 'package:tok_tik/presentation/widgets/shared/video_buttons.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  const VideoScrollableView({super.key, required this.videos});
+  final List<VideoPost> videos;
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            // video player
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButtons(video: videoPost),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
