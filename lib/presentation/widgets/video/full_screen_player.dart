44a0806902c2ae4afc8b44142a0948a05752e182
nblancoz <nblancozappala@gmail.com>
2024-08-03 00:05:49 +0200

    caption & play/pause video added

--- lib/presentation/widgets/video/full_screen_player.dart
+++ lib/presentation/widgets/video/full_screen_player.dart
@@ -46,11 +46,53 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
             ),
           );
         }
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: VideoPlayer(controller),
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+                VideoPlayer(controller),
+                // gradiente
+                Positioned(
+                  bottom: 50,
+                  left: 20,
+                  child: _VideoCaption(
+                    caption: widget.caption,
+                  ),
+                )
+              ],
+            ),
+          ),
         );
       },
+    );
+  }
+}
+
+class _VideoCaption extends StatelessWidget {
+  const _VideoCaption({required this.caption});
+
+  final String caption;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return SizedBox(
+      width: size.width * 0.6,
+      child: Text(
+        caption,
+        maxLines: 2,
+        style: titleStyle,
+      ),
     );
   }
 }
