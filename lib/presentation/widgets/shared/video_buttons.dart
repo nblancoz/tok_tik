bb486bea070b98d4f5dcdefada9d80b9e50486a6
nblancoz <nblancozappala@gmail.com>
2024-08-02 21:18:16 +0200

    added new button with animation

--- lib/presentation/widgets/shared/video_buttons.dart
+++ lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:tok_tik/config/helpers/human_formats.dart';
 import 'package:tok_tik/domain/entities/video_post.dart';
@@ -15,9 +16,19 @@ class VideoButtons extends StatelessWidget {
           iconColor: Colors.red,
           iconData: Icons.favorite,
         ),
+        const SizedBox(height: 20),
         _CustomIconButton(
           value: video.views,
           iconData: Icons.remove_red_eye_outlined,
+        ),
+        const SizedBox(height: 20),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5),
+          child: const _CustomIconButton(
+            value: 0,
+            iconData: Icons.play_circle_outline,
+          ),
         ),
       ],
     );
@@ -47,7 +58,7 @@ class _CustomIconButton extends StatelessWidget {
             size: 30,
           ),
         ),
-        Text(HumanFormats.humanReadableNumber(value.toDouble()))
+        if (value > 0) Text(HumanFormats.humanReadableNumber(value.toDouble()))
       ],
     );
   }
